8b1b926931cb8fd6263264d921329298126f4e37
araelath <jeanb.rocher@gmail.com>
2016-02-26 11:48:18 +0100

    fuck val

--- node/socketserver.js
+++ node/socketserver.js
@@ -1,9 +1,36 @@
+
+var $ = require('jquery')(require('jsdom').jsdom().parentWindow);
+
 var io = require('socket.io').listen(8000);
 
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    socket.emit('news', {connard: 'pd'});
-    console.log('je passe ici');
-  });
+io.sockets.on('connection', function (socket){
+
+  $.getJSON( "taxis.json", function( data ) {
+     var res = [];
+     var i = 0;
+     for(i=1; i< data.length;i++){
+       var taxi = data[i];
+
+       if(typeof taxi.geometry=="undefined"){
+         console.log(i);
+         console.log(taxi);
+
+       }
+       else{
+
+         var el = {
+          id: taxi.fields.station_id,
+          lon: taxi.geometry.coordinates[0],
+          lat: taxi.geometry.coordinates[1]
+
+         };
+         res.push(el);
+       }
+     }
+
+    socket.emit('news', res);
+
+   });
+
+
 });
